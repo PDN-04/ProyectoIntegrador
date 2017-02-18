d22d21e47719049abfb3d85c60665310171d1ce4
PDN-04 <domingueznavarropablo@gmail.com>
2017-02-18 20:01:05 +0100

    Entrega Bloques 1, 2 y 3

--- js/main.js
+++ js/main.js
@@ -1,36 +1,48 @@
-$(document).ready(main);
-var mover = true;
-function main() {
-	$(".boton").click(function(){
-		if (mover) {
-			$("nav").animate({
-				left: "0px"
-			});
-			mover = false;
-		}
-		else {
-			$("nav").animate({
-				left: "-100%"
-			});
-			mover = true;
-		}
-	});
-	$(".boton-inferior").click(function(){
-		if (mover) {
-			$("nav").animate({
-				left: "0px"
-			});
-			mover = false;
-		}
-		else {
-			$("nav").animate({
-				left: "-100%"
-			});
-			mover = true;
-		}
-	});
-	$(".submenu").click(function(){
-		$(this).children(".children").slideToggle();
-		$(this).toggleClass('girar');
-	});
-}
+$(document).ready(function() {
+    $(".button").click(function() {
+        $('body').animate({
+            scrollTop: $(".pag2").offset().top
+        }, 'slow');
+    });
+    $(".estatura").on("input", function() {
+        variable = this.value;
+        if (variable == $(".estatura").prop('max')) {
+            $("#estatura").text("> " + variable + " cm");
+        } else {
+            if (variable == $(".estatura").prop('min')) {
+                $("#estatura").text("< " + variable + " cm");
+            } else {
+                $("#estatura").text(variable + " cm");
+            }
+        }
+
+    });
+    $(".peso").on("input", function() {
+        variable = this.value;
+        if (variable == $(".peso").prop('max')) {
+            $("#peso").text("> " + variable + " kg");
+        } else {
+            if (variable == $(".peso").prop('min')) {
+                $("#peso").text("< " + variable + " kg");
+            } else {
+                $("#peso").text(variable + " kg");
+            }
+        }
+
+    });
+    $(".opciones div").hide();
+    $("#tabs li:first").attr("id", "current");
+    $(".opciones div:first").fadeIn();
+
+    $('#tabs a').click(function(evento) {
+        evento.preventDefault();
+        $(".opciones div").hide();
+        $("#tabs li").attr("id", "");
+        $(this).parent().attr("id", "current");
+        $('#' + $(this).attr('class')).fadeIn();
+    });
+    $('#calcular').click(function(evento) {
+        evento.preventDefault();
+        // $('#sprint').val($('#dia').val());
+    });
+});
